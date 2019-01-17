f6fda05aa1a0a57813382ac3d6f34ccd600d024c
John Doe <johndoe@example.com>
2019-01-16 22:34:22 -0500

    web

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ $('[data-filter="all"]').click(() => {
     $('[data-multifilter]').prop('checked', false);
 })
 
+new WOW().init();
+
+// $(window).on('scroll', function() {
+//     if ($(window).scrollTop()){
+//         $('navbar').addClass('black');
+//     }
+//     else{ $('navbar').removeClass('black');
+// }
+// })
